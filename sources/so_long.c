0708ef44017ea7b31b276b98a75712f20ba868ce
Gogiu23 <giuliandominici@gmail.com>
2022-12-03 14:12:18 +0100

    leaks on the main and the array

--- sources/so_long.c
+++ sources/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gdominic <gdominic@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 16:00:14 by gdominic          #+#    #+#             */
-/*   Updated: 2022/12/01 22:03:36 by gdominic         ###   ########.fr       */
+/*   Updated: 2022/12/03 14:11:10 by gdominic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 #include "../libft/includes/libft.h"
 #include "../mlx/mlx.h"
 
-//int	main(int argc, char **argv)
-//{
-//	t_data	data;
-//	t_data	t;
-//	int		fd;
-//
-//	if (argc == 1)
-//		return (0);
-//	fd = open(argv[1], O_RDONLY);
-//	ft_fitoar(&t, fd);
-//	ft_print_stack(&t);
-//	data.x = 5;
-//	data.y = 5;
-//	data.mlx = mlx_init();
-//	data.win = mlx_new_window(data.mlx, 1240, 720, "Hello world");
+int	main(int argc, char **argv)
+{
+	t_data	data;
+	t_data	t;
+	int		fd;
+ 	char	*relative_path = "../images/image-1.xpm";
+
+	if (argc == 1)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	ft_fitoar(&t, fd);
+	ft_print_stack(&t);
+	data.x = 5;
+	data.y = 5;
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, 1240, 720, "Hello world");
+	data.img = mlx_xpm_file_to_image(
 //	data.img = mlx_new_image(data.mlx, 1240, 720);
 //	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, \
 //			&data.line_length, &data.endian);
@@ -36,31 +38,7 @@
 //		ft_square(&data);
 //	if (ft_strncmp(argv[1], "triangle", 8) == 0)
 //		ft_triangle(&data);
-//	close(fd);
-//	ft_free_stacks_t(&t);
-//	return (0);
-//}
-typedef struct s_vars {
-	void	*mlx;
-	void	*win;
-}	t_vars;
-
-int	key_hook(int keycode, t_vars *vars)
-{
-//	if (keycode == 0)
-//		vars = 0;
-	vars = 0;
-	if (keycode == 0)
-		printf("Hello from key_hook");
+	close(fd);
+	ft_free_stacks_t(&t);
 	return (0);
-}
-
-int	main(void)
-{
-	t_vars	vars;
-
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world");
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_loop(vars.mlx);
 }
